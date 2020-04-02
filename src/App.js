e29e3a302cfe2015d87db3d934b072d54e0cc082
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-02 21:52:20 +1100

    Hello

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App-header">
+      <header>
+        <p> Ken was here</p>
       </header>
+      <a className="App-link"
+        href="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/577566_624080227620536_1733787268_n.jpg?_nc_cat=103&_nc_sid=110474&_nc_ohc=aepL1lIw-F0AX_vahy0&_nc_ht=scontent.fsyd8-1.fna&oh=0c4ab8c38187a3935bc6e8c8b7a247b4&oe=5EAB6220">
+        Finished project here - Ken
+      </a>
     </div>
   );
 }

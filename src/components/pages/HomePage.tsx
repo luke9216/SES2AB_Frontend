caea9f4fe2429c2de23c015ab483e0c15243ca16
Luke Phillips <lukephillips9216@gmail.com>
2020-04-07 16:28:29 +1000

    Done navigation buttons

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import { Typography } from '@material-ui/core';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Done navigation buttons
 import { RouteComponentProps } from 'react-router';
 import NavigationButtons, { NavigationButtonsTab } from "../common/NavigationButtons"
 import navigationTabs from "../common/__data__/data.navigationTabs.json"
 import logo from '../../images/uts-logo.png';
 import { logoStyles } from "../common/styles"
+<<<<<<< HEAD
 
 export interface HomePageProps {
     props?: any;
@@ -21,15 +25,27 @@ const HomePage: React.SFC<HomePageProps> = ({ props }) => {
             <Typography variant="h3">UTS: Quantum Computing</Typography>
         </NavigationButtons>
 =======
+=======
+>>>>>>> Done navigation buttons
 
 export interface HomePageProps {
-
+    props?: any;
+    match?: RouteComponentProps;
 }
 
-const HomePage: React.SFC<HomePageProps> = () => {
+const HomePage: React.SFC<HomePageProps> = ({ props }) => {
+    const classes = logoStyles();
+    const { path } = props.match;
     return (
+<<<<<<< HEAD
         <Typography variant="h2">UTS: Quantum Computing</Typography>
 >>>>>>> Set up inital home page and project structure
+=======
+        <NavigationButtons path={path} navigationTabs={navigationTabs as Array<NavigationButtonsTab>}>
+            <img src={logo} className={classes.logo} alt="UTS Logo" />
+            <Typography variant="h3">UTS: Quantum Computing</Typography>
+        </NavigationButtons>
+>>>>>>> Done navigation buttons
     );
 }
 
--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -5,8 +5,12 @@ import HomePage from "./HomePage"
 import LoginPage from "./LoginPage"
 =======
 import HomePage from "./HomePage"
+<<<<<<< HEAD
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 >>>>>>> Set up inital home page and project structure
+=======
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+>>>>>>> Done navigation buttons
 
 export interface RoutingProps {
 
@@ -17,11 +21,15 @@ const Routing: React.SFC<RoutingProps> = () => {
         <Router>
             <Switch>
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
                 <Route path="/login" render={(props: any) => <LoginPage props={props} />} />
 =======
                 <Route path="/" exact={true} render={() => <HomePage />} />
 >>>>>>> Set up inital home page and project structure
+=======
+                <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
+>>>>>>> Done navigation buttons
             </Switch>
         </Router>
     );

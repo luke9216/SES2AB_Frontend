0896a1dec817ed79213346502ebd8ceaf4531392
MitchellUTS <13248202@student.uts.edu.au>
2020-04-08 16:19:09 +1000

    Added a link to the API
    Added a link to the API

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
       <header>
         <p> Ken was here</p>
       </header>
@@ -14,6 +15,17 @@ function App() {
         href="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/577566_624080227620536_1733787268_n.jpg?_nc_cat=103&_nc_sid=110474&_nc_ohc=aepL1lIw-F0AX_vahy0&_nc_ht=scontent.fsyd8-1.fna&oh=0c4ab8c38187a3935bc6e8c8b7a247b4&oe=5EAB6220">
         Finished project here - Ken
       </a>
+      <p>
+        Example <code>React.js</code> Application for Quantum UI site.
+      </p>
+      <a
+        className="App-link"
+        href="https://api.ses2a.zyrn.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check out SES2A's API
+      </a>      
     </div>
   );
 }

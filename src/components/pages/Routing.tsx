2bcdd4f1bed9d9a1de0e1ec44150d3b22991389e
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-16 19:22:14 +1000

    hello hello

--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 <<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -7,12 +8,17 @@ import LoginPage from "./LoginPage"
 import HomePage from "./HomePage"
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 >>>>>>> Set up inital home page and project structure
+=======
+import React from "react";
+import HomePage from "./HomePage";
+import ToolBox from "./ToolBox/ToolBox";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+>>>>>>> hello hello
 
-export interface RoutingProps {
-
-}
+export interface RoutingProps {}
 
 const Routing: React.SFC<RoutingProps> = () => {
+<<<<<<< HEAD
     return (
         <Router>
             <Switch>
@@ -26,5 +32,24 @@ const Routing: React.SFC<RoutingProps> = () => {
         </Router>
     );
 }
+=======
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/"
+          exact={true}
+          render={(props: any) => <HomePage props={props} />}
+        />
+        <Route
+          path="/toolbox"
+          exact={true}
+          render={(props: any) => <ToolBox props={props} />}
+        />
+      </Switch>
+    </Router>
+  );
+};
+>>>>>>> hello hello
 
 export default Routing;
---
+++ src/components/pages/ToolBox/ToolBox.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface ToolBoxProps {
+  props?: any;
+}
+
+const ToolBox: React.SFC<ToolBoxProps> = () => {
+  return (
+    <div>
+      <a>Renz is gay</a>
+    </div>
+  );
+};
+
+export default ToolBox;

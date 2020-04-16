130ffea77bf770d715cdcabc4ff798aa6c1bb80d
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-16 19:22:14 +1000

    hello hello

--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import HomePage from "./HomePage"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import HomePage from "./HomePage";
+import ToolBox from "./ToolBox/ToolBox";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export interface RoutingProps {
-
-}
+export interface RoutingProps {}
 
 const Routing: React.SFC<RoutingProps> = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
-            </Switch>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/"
+          exact={true}
+          render={(props: any) => <HomePage props={props} />}
+        />
+        <Route
+          path="/toolbox"
+          exact={true}
+          render={(props: any) => <ToolBox props={props} />}
+        />
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routing;

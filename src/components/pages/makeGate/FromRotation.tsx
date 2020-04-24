08a72d6fdf6f4422b5cc39410431b297722f28ec
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-24 17:53:30 +1000

    Update: Need to properly update states

--- src/components/pages/makeGate/FromRotation.tsx
+++ src/components/pages/makeGate/FromRotation.tsx
@@ -4,13 +4,15 @@ import Grid from "@material-ui/core/Grid";
 import { Button, TextField } from "@material-ui/core";
 
 interface FromRotationProps {
+  axis: string;
   handleChange: Function;
-  handleClick: any;
+  handleSubmit: Function;
 }
 
 const FromRotation: React.FunctionComponent<FromRotationProps> = ({
+  axis,
   handleChange,
-  handleClick,
+  handleSubmit,
 }) => {
   const classes = paperStyles();
 
@@ -21,39 +23,29 @@ const FromRotation: React.FunctionComponent<FromRotationProps> = ({
       </Grid>
       <Grid item xs={12}>
         <TextField
+          id={axis}
           label="Axis"
+          name={axis}
           placeholder="X+Y"
           variant="outlined"
-          onChange={handleChange()}
+          onChange={handleChange(axis)}
         />
       </Grid>
       <Grid item xs={12}>
-        <TextField
-          id="outlined-basic"
-          label="Angle"
-          placeholder="45"
-          variant="outlined"
-        />
+        <TextField label="Angle" placeholder="45" variant="outlined" />
       </Grid>
       <Grid item xs={12}>
-        <TextField
-          id="outlined-basic"
-          label="Global Phase"
-          placeholder="0"
-          variant="outlined"
-        />
+        <TextField label="Global Phase" placeholder="0" variant="outlined" />
       </Grid>
-
       <text> placeholder</text>
       <Grid item xs={12}>
         <TextField
-          id="outlined-basic"
           label="Circuit Symbol"
           placeholder="[The matrix]"
           variant="outlined"
         />
       </Grid>
-      <Button variant="contained" color="primary" onClick={handleClick()}>
+      <Button variant="contained" color="primary" onClick={handleSubmit()}>
         Create Rotation Gate
       </Button>
     </Grid>
--- src/components/pages/makeGate/MakeGate.tsx
+++ src/components/pages/makeGate/MakeGate.tsx
@@ -5,25 +5,66 @@ import FromCircuit from "./FromCircuit";
 import FromMatrix from "./FromMatrix";
 import Grid from "@material-ui/core/Grid";
 import { paperStyles } from "./styles";
+import { TextField, Button } from "@material-ui/core";
+
+/* const rotationStates = {
+  axis: "",
+  angle: "",
+} as IRotationDetails;
+
+const [values, setValues] = useState<IRotationDetails>(rotationStates);
+
+  const onRotationChange = (details: string) => (event: any) => {
+    details = event.target.value;
+
+    setValues({
+      ...values,
+      [details]: event.target.value,
+    });
+  }; 
+  export interface IRotationDetails {
+    axis: string;
+    angle: string;
+  }
+  
+  */
 
 export interface MakeGateProps {
   props?: any;
-  match?: RouteComponentProps;
-  axis?: String;
-  angle?: String;
 }
 
-const printValue = () => (event: any) => {
-  var message: string = "RENZ WAS HERE";
-  console.log(message);
-};
+const MakeGate: React.FunctionComponent<MakeGateProps> = () => {
+  const [state, setState] = useState({
+    testaxis: "test axis",
+    angle: "test angle",
+  });
 
-export const MakeGate: React.FunctionComponent<MakeGateProps> = () => {
+  const handleChange = () => (event: any) => {
+    const value = event.target.value;
+    console.log(value);
+    setState({
+      ...state,
+      [event.target.name]: value,
+    });
+    console.log(event.target.name);
+  };
+
+  const onAxisChange = (details: string) => (event: any) => {
+    details = event.target.value;
+    state.testaxis = details;
+    console.log(state.testaxis);
+  };
+
+  const onSubmit = () => (event: any) => {
+    console.log(state.testaxis);
+  };
   const classes = paperStyles();
-  const [axis, setAxis] = useState(0);
-
   return (
     <div>
+      <input name={state.testaxis} onChange={handleChange()}></input>
+      <h1>break</h1>
+      <TextField name={state.angle} onChange={handleChange()}></TextField>
+      <Button onClick={onSubmit()}>click</Button>
       <Grid
         container
         justify="flex-start"
@@ -32,7 +73,11 @@ export const MakeGate: React.FunctionComponent<MakeGateProps> = () => {
         spacing={1}
       >
         <Grid item xs={3}>
-          <FromRotation handleChange={setAxis} handleClick={printValue} />
+          <FromRotation
+            axis={state.testaxis}
+            handleChange={handleChange}
+            handleSubmit={onSubmit}
+          />
         </Grid>
         <Grid item xs={3}>
           <FromMatrix />

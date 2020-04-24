b196d60ec69398641968df0055abbbf3d3dd5805
Renzeu <renzeu.sin@gmail.com>
2020-04-24 14:21:36 +1000

    Tooltips test

--- src/components/pages/ToolBox/GateTypes.tsx
+++ src/components/pages/ToolBox/GateTypes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Tooltip, Button  } from "@material-ui/core";
 
 export interface GateTypeProps {
   types: Array<GateTypeTab>;
@@ -11,17 +11,11 @@ export interface GateTypeTab {
 
 const GateTypes: React.SFC<GateTypeProps> = ({ types }) => {
   return (
-    <Grid container direction="row" spacing={10}>
-      {types.map((tab: GateTypeTab, index: number) => {
-        return (
-          <div>
-            <Grid item spacing={0}>
-              <h2>{tab.name}</h2>
-            </Grid>
-          </div>
-        );
-      })}
-    </Grid>
+      <div> 
+      <Tooltip title="Measures whether a qubit is ON or OFF, condition on the result">
+        <Button>Measurement</Button>
+      </Tooltip>
+      </div>
   );
 };
 

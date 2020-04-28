42f19e72d74a2491671edd8be605a7d18290fc03
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-28 20:11:23 +1000

    Update: changed toolbox

--- src/components/pages/ToolBox/GateTypes.tsx
+++ src/components/pages/ToolBox/GateTypes.tsx
@@ -1,21 +1,33 @@
 import * as React from "react";
-import { Grid, Tooltip, Button  } from "@material-ui/core";
+import { Grid, Button, Tabs, Tooltip } from "@material-ui/core";
 
-export interface GateTypeProps {
-  types: Array<GateTypeTab>;
+interface GateTypesProps {
+  gateTypes: Array<GateButtonTab>;
 }
 
-export interface GateTypeTab {
+export interface GateButtonTab {
   name: string;
+  gates: Array<String>;
+  description: string;
 }
-
-const GateTypes: React.SFC<GateTypeProps> = ({ types }) => {
+const GateTypes: React.SFC<GateTypesProps> = ({ gateTypes }) => {
   return (
-      <div> 
-      <Tooltip title="Measures whether a qubit is ON or OFF, condition on the result">
-        <Button>Measurement</Button>
-      </Tooltip>
-      </div>
+    <React.Fragment>
+      {gateTypes.map((tab: GateButtonTab, i) => (
+        <Grid key={i} item direction="row">
+          <h1>{tab.gates}</h1>
+          <Grid container spacing={2}>
+            {tab.gates.map((gate, j) => (
+              <Grid item spacing={1} xs>
+                <Button key={j} variant="contained">
+                  {gate}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      ))}
+    </React.Fragment>
   );
 };
 

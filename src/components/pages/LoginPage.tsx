012365a1a760cd4c7fda31b61a508fa49d97c40d
Luke Phillips <lukephillips9216@gmail.com>
2020-04-28 20:48:01 +1000

    mock login ui

---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { useHistory } from 'react-router-dom';
+import LoginForm from './../containers/LoginForm';
+
+export interface LoginPageProps {
+    props?: any
+}
+
+const LoginPage: React.SFC<LoginPageProps> = (props) => {
+    const history = useHistory();
+    const [userDetailsState, setUserDetailsState] = React.useState({
+        email: "",
+        password: ""
+    });
+
+    const onEmailChange = () => (event: any) => {
+        setUserDetailsState({
+            ...userDetailsState,
+            email: event.target.value
+        })
+    };
+
+    const onPasswordChange = () => (event: any) => {
+        setUserDetailsState({
+            ...userDetailsState,
+            password: event.target.value
+        })
+    };
+
+    const onLogginBtnClicked = () => (event: any) => {
+        if (userDetailsState.email.toLowerCase() === "test@test.com" && userDetailsState.password.toLowerCase() === "12345") {
+            console.log("Login Successful")
+            window.location.href = 'http://localhost:3000/';
+        } else {
+            console.log("Login Failed")
+        }
+    }
+
+    return (
+        <LoginForm handleEmailChange={onEmailChange} handlePasswordChange={onPasswordChange} handleSubmitBtnClick={onLogginBtnClicked} />
+    );
+};
+
+export default LoginPage;

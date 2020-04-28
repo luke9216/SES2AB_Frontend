ebc7bc8a08b9eea99a67fc6b2cd08a76f886272f
Luke Phillips <lukephillips9216@gmail.com>
2020-04-28 20:48:01 +1000

    mock login ui

--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,38 +1,26 @@
-import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from "./HomePage"
-import LoginPage from "./LoginPage"
-=======
-import HomePage from "./HomePage"
-<<<<<<< HEAD
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
->>>>>>> Set up inital home page and project structure
-=======
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
->>>>>>> Done navigation buttons
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
 
-export interface RoutingProps {
-
-}
+export interface RoutingProps {}
 
 const Routing: React.SFC<RoutingProps> = () => {
-    return (
-        <Router>
-            <Switch>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
-                <Route path="/login" render={(props: any) => <LoginPage props={props} />} />
-=======
-                <Route path="/" exact={true} render={() => <HomePage />} />
->>>>>>> Set up inital home page and project structure
-=======
-                <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
->>>>>>> Done navigation buttons
-            </Switch>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path="/"
+          exact={true}
+          render={(props: any) => <HomePage props={props} />}
+        />
+        <Route
+          path="/login"
+          render={(props: any) => <LoginPage props={props} />}
+        />
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routing;

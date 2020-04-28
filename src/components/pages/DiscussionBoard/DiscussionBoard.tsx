24ca81bbca92f77246ad7661df6a1f1ce38de5a5
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-28 20:08:47 +1000

    Added templates for the drawer

---
+++ src/components/pages/DiscussionBoard/DiscussionBoard.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface DiscussionBoardProps {}
+
+const DiscussionBoard: React.SFC = () => {
+  return <div></div>;
+};
+
+export default DiscussionBoard;
---
+++ src/components/pages/Workshop/Workshop.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface WorkshopProps {}
+
+const Workshop: React.SFC = () => {
+  return <div></div>;
+};
+
+export default Workshop;

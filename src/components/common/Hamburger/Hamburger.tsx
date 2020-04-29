9462b83183f177bba3b3e8086e1cd5dbc7d1b30d
harrisontruong1 <h.vstruong@gmail.com>
2020-04-29 22:34:52 +1000

    assignments update

--- src/components/common/Hamburger/Hamburger.tsx
+++ src/components/common/Hamburger/Hamburger.tsx
@@ -1,108 +1,15 @@
 import * as React from "react";
 import clsx from "clsx";
-import { hamburgerStyles } from "./style";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Icon from "@material-ui/core/Icon";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import IconButton from "@material-ui/core/IconButton";
-import { useTheme } from "@material-ui/core/styles";
 
 export interface HamburgerProps {
   props?: any;
 }
 
 const Hamburger: React.SFC<HamburgerProps> = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const classes = hamburgerStyles();
-  const theme = useTheme();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-            <Typography variant="h6" noWrap>
-              Your Navigation
-            </Typography>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {["Assignments", "Discussion Board", "Your Workshop", "Lectures"].map(
-            (text, index) => (
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
-              </ListItem>
-            )
-          )}
-        </List>
-        <Divider />
-        <List>
-          {["Settings", "Contact", "Log Out"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+    <div>
+
     </div>
   );
 };
--- src/components/pages/Assignments/style.ts
+++ src/components/pages/Assignments/style.ts
@@ -1,5 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
+interface Props {
+  color: 'red' | 'blue';
+}
+
 export const assignmentStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -28,4 +32,20 @@ export const assignmentStyles = makeStyles((theme) => ({
     background: "red",
     border: "solid",
   },
+  title1: {
+    background: (props: Props) =>
+    props.color === 'red'
+      ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+      : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: (props: Props) =>
+    props.color === 'red'
+      ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
+      : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  margin: 8,
+  }
 }));

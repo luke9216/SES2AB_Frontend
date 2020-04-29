effc5443783a311202a3f44e5ebf7ed73d6b4d5a
jyotsnabali <49472813+jyotsnabali@users.noreply.github.com>
2020-04-29 22:52:02 +1000

    update discussion board
    update discussion board

--- src/components/pages/DiscussionBoard/style.ts
+++ src/components/pages/DiscussionBoard/style.ts
@@ -3,14 +3,23 @@ import { makeStyles } from "@material-ui/core/styles";
 export const discussionBoardStyles = makeStyles((theme) => ({
   header: {
     border: "solid",
-    background: "grey",
+    background: "white",
   },
   tableTitle: {
-    color: "blue",
+    color: "white",
     marginLeft: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "darkmagenta",
   },
   rowStyle: {
-    color: "grey",
+    color: "darkmagenta",
+    background: "gainsboro",
+  },
+  tableStyle: {
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   paper: {
     padding: theme.spacing(5),

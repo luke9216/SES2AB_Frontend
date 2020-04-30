ce104ede028b886eeda5177b0af8995419c481c6
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-30 13:38:24 +1000

    Merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Routing from "./components/pages/Routing";
 import "./App.css";
-import Hamburger from "./components/common/Hamburger/Hamburger";
 
 export interface AppProps {}
 
 const App: React.SFC<AppProps> = () => {
-  return (
-    <Routing>
-      <Hamburger />
-    </Routing>
-  );
+  return <Routing></Routing>;
 };
 
 export default App;
--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import HomePage from "./HomePage";
-import ToolBox from "./ToolBox/ToolBox";
 import Assignment from "./Assignments/Assignment";
-import DiscussionBoard from "./DiscussionBoard/DiscussionBoard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Hamburger from "./../common/Hamburger/Hamburger";
 
 export interface RoutingProps {}
 
@@ -18,24 +15,9 @@ const Routing: React.SFC<RoutingProps> = () => {
           render={(props: any) => <HomePage props={props} />}
         />
         <Route
-          path="/toolbox"
-          exact={true}
-          render={(props: any) => <ToolBox props={props} />}
-        />
-        <Route
-          path="/discuss"
-          exact={true}
-          render={(props: any) => <DiscussionBoard />}
-        />
-        <Route
           path="/assignment"
           exact={true}
           render={(props: any) => <Assignment />}
-        />
-        <Route
-          path="/drawer"
-          exact={true}
-          render={(props: any) => <Hamburger />}
         />
       </Switch>
     </Router>

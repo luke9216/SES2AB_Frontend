12ae26a256fb0109050b2700652b24569c445e22
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-30 13:07:41 +1000

    sd

--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,45 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from "./HomePage"
-import LoginPage from "./LoginPage"
-=======
-import HomePage from "./HomePage"
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
->>>>>>> Set up inital home page and project structure
-=======
 import React from "react";
-import HomePage from "./HomePage";
-import DiscussionBoard from "./DiscussionBoard/DiscussionBoard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> hello hello
-=======
-import Hamburger from "./../common/Hamburger/Hamburger";
->>>>>>> AAAAAAAAAAAAH
-=======
->>>>>>> Update: beginning merge
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
 
 export interface RoutingProps {}
 
 const Routing: React.SFC<RoutingProps> = () => {
-<<<<<<< HEAD
-    return (
-        <Router>
-            <Switch>
-<<<<<<< HEAD
-                <Route path="/" exact={true} render={(props: any) => <HomePage props={props} />} />
-                <Route path="/login" render={(props: any) => <LoginPage props={props} />} />
-=======
-                <Route path="/" exact={true} render={() => <HomePage />} />
->>>>>>> Set up inital home page and project structure
-            </Switch>
-        </Router>
-    );
-}
-=======
   return (
     <Router>
       <Switch>
@@ -49,14 +15,12 @@ const Routing: React.SFC<RoutingProps> = () => {
           render={(props: any) => <HomePage props={props} />}
         />
         <Route
-          path="/discuss"
-          exact={true}
-          render={(props: any) => <DiscussionBoard />}
+          path="/login"
+          render={(props: any) => <LoginPage props={props} />}
         />
       </Switch>
     </Router>
   );
 };
->>>>>>> hello hello
 
 export default Routing;

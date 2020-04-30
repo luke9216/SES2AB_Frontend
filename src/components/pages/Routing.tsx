d5e5727241fc76def51f1b74589a420d34877034
KenPhetmany <kphetmany@student.uts.edu.au>
2020-04-30 13:47:05 +1000

    Merging

--- src/components/pages/Routing.tsx
+++ src/components/pages/Routing.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
-import ToolBox from "./ToolBox/ToolBox";
 import { HamburgerRoutes } from "./../common/Hamburger/HamburgerRoutes";
 import HomePage from "./HomePage";
 
@@ -10,7 +9,6 @@ export const Routing: React.SFC<RoutingProps> = () => (
   <BrowserRouter>
     <Switch>
       <HamburgerRoutes exact path="/" screen={HomePage} />
-      <HamburgerRoutes exact path="/toolbox" screen={ToolBox} />
     </Switch>
   </BrowserRouter>
 );

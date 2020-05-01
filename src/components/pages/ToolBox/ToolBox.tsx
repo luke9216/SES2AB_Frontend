9b7eaa4d11edb49ca8054461c2801f97bcde59a9
Ken Phetmany <Ken@Kens-MacBook-Pro.local>
2020-05-01 20:03:04 +1000

    Update: implemented drag and drop feature. For now I can only add one gate at a time

--- src/components/pages/ToolBox/ToolBox.tsx
+++ src/components/pages/ToolBox/ToolBox.tsx
@@ -1,65 +1,44 @@
 import * as React from "react";
-import { Grid, Divider } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import GateTypes, { GateButtonTab } from "./GateTypes";
 import { paperStyles } from "./styles";
 import gateTypes from "../../common/__data__/data.gateTypes.json";
-import { Typography, Link, Box, Container } from "@material-ui/core";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export interface ToolBoxProps {
   props?: any;
 }
 
-/* 
-    if (tab.name == id) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      tab.category == cat;
-    }
-    return tab;
-*/
-
-const onDragStart = (event: any, id: any) => {
-  console.log("drag start", id);
-  event.dataTransfer.setData("id", id);
-};
-
-const onDragOver = (event: any) => {
-  event.preventDefault();
-  console.log("drag over");
-};
-
 export interface ICircuitBoard {
   using: Array<String>;
 }
 
-const circuitBoard = {
-  using: [],
-} as ICircuitBoard;
-
-const onDrop = (event: any, cat: any) => {
-  let id = event.dataTransfer.getData("id");
-
-  gateTypes.map((tab: GateButtonTab) => {
-    let gates = tab.gates.filter((gate) => {
-      if (gate == id) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        gate === cat;
-      }
-      return gate;
-    });
-    circuitBoard.using = gates;
-    console.log("circuit board:", circuitBoard.using);
-  });
-
-  //console.log("Circuit:", name);
-  //console.log("Gate Name:", id);
-  //console.log("Array:", circuitBoard.using);
-};
-
 const ToolBox: React.SFC<ToolBoxProps> = () => {
+  const [circuitState, setCircuit] = React.useState({
+      circuit: []
+  });
+  
+  
+
+  const onDragStart = (event: any, id: any) => {
+    console.log("drag start", id);
+    event.dataTransfer.setData("id", id);
+  };
+  
+  const onDragOver = (event: any) => {
+    event.preventDefault();
+  };
+  
+  const onDrop = (event: any, cat: any) => {
+    let id = event.dataTransfer.getData("id");
+
+    setCircuit({
+      ...circuitState,
+      circuit: id
+  })
+
+    console.log("gate", id)
+  };
+
   const classes = paperStyles();
   return (
     <div>
@@ -84,7 +63,7 @@ const ToolBox: React.SFC<ToolBoxProps> = () => {
             onDrop={(e) => onDrop(e, "circuits")}
             className={classes.circuit}
           >
-            CircuitBoards; {circuitBoard.using}
+            CircuitBoards: {circuitState.circuit}
           </div>
         </Grid>
       </div>

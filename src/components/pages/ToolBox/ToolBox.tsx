06b3a6d6f319d8436de19e75f6b74558b9f18a20
KenPhetmany <kphetmany@student.uts.edu.au>
2020-05-04 15:59:32 +1000

    Update: Succesffuly implemented a delete feature

--- src/components/pages/ToolBox/ToolBox.tsx
+++ src/components/pages/ToolBox/ToolBox.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 import GateTypes, { GateButtonTab } from "./GateTypes";
 import { paperStyles } from "./harryStyles";
 import gateTypes from "../../common/__data__/data.gateTypes.json";
@@ -9,32 +9,40 @@ export interface ToolBoxProps {
 }
 
 export interface ICircuitBoard {
-  using: Array<String>;
+  circuit: Array<string>;
 }
 
 const ToolBox: React.SFC<ToolBoxProps> = () => {
   const [circuitState, setCircuit] = React.useState({
-      circuit: []
-  });
+    circuit: [""],
+  } as ICircuitBoard);
 
   const onDragStart = (event: any, id: any) => {
     console.log("drag start", id);
     event.dataTransfer.setData("id", id);
   };
-  
+
   const onDragOver = (event: any) => {
     event.preventDefault();
   };
-  
+
   const onDrop = (event: any) => {
     let id = event.dataTransfer.getData("id");
 
     setCircuit({
       ...circuitState,
-      circuit: circuitState.circuit.concat(id)
-  })
+      circuit: circuitState.circuit.concat(id),
+    });
 
-    console.log("gate", id)
+    console.log("gate", id);
+  };
+
+  const onDelete = (event: any, id: string) => {
+    const items = circuitState.circuit.filter((gate: string) => gate !== id);
+    setCircuit({
+      ...circuitState,
+      circuit: items,
+    });
   };
 
   const classes = paperStyles();
@@ -50,13 +58,15 @@ const ToolBox: React.SFC<ToolBoxProps> = () => {
           ;
         </Grid>
         <h1 className={classes.title1}>Circuit</h1>
-          <div
-            onDragOver={(e) => onDragOver(e)}
-            onDrop={(e) => onDrop(e)}
-            className={classes.circuit}
-          >
-            CircuitBoards: {circuitState.circuit} 
-          </div>
+        <div
+          onDragOver={(e) => onDragOver(e)}
+          onDrop={(e) => onDrop(e)}
+          className={classes.circuit}
+        >
+          {circuitState.circuit.map((item) => (
+            <Button onClick={(event) => onDelete(event, item)}>{item}</Button>
+          ))}
+        </div>
       </div>
     </div>
   );

8e9459ccdc95723b3787c833f0f09edba8504fbf
KenPhetmany <kphetmany@student.uts.edu.au>
2020-05-19 17:58:40 +1000

    Update: Changed homepage and colour of discussion board

--- src/components/pages/Homepage/Bulletin.tsx
+++ src/components/pages/Homepage/Bulletin.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
+import { Grid } from "@material-ui/core";
 
-export interface BulletinProps { }
+export interface BulletinProps {}
 
 const Bulletin: React.SFC<BulletinProps> = () => {
-    return (
-        <div>
-            This is the Bulletin Module
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is the Bulletin Module</h1>
+      <Grid container>
+        <h2>Heres what's new!</h2>
+      </Grid>
+    </div>
+  );
 };
 
 export default Bulletin;

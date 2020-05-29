2a5629ddc9a520b08a96b5b33212f556757ac886
Renzeu <renzeu.sin@gmail.com>
2020-05-29 13:02:23 +1000

    Changed resourceform title section to take in categories from a dropdown select

--- src/components/pages/Resources/ResourcesForm.tsx
+++ src/components/pages/Resources/ResourcesForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, TextField, MenuItem } from "@material-ui/core";
 import { resourcesStyles } from "./style";
 
 export interface ResourcesFormProps {
@@ -14,26 +14,34 @@ const Resources: React.SFC<ResourcesFormProps> = () => {
   const classes = resourcesStyles();
 
   return (
-    
     <div>
       <h1 className={classes.title1}>Upload Form</h1>
-        <Grid container direction = "column" justify = "center" alignItems="center">
-            <label>
-              <h2 className={classes.title1}>File Title</h2>
-              <input type="text" placeholder="Enter title for upload"/>
-            </label>
+      
+      <Grid container direction = "column" justify = "center" alignItems="center">
+          <label>
+            <h2 className={classes.title1}>Category</h2>
 
-            <label>
+            <TextField id="select" fullWidth={true} value="textbook" select>
+              <MenuItem value="textbook">Textbook</MenuItem>
+              <MenuItem value="lectures">Lectures</MenuItem>
+              <MenuItem value="videos">Videos</MenuItem>
+            </TextField>
+          </label>
+
+          <label>
             <h2 className={classes.title1}>File</h2>
-              <input type="file" name="file"/>
-            </label>
+
+            <input type="file" name="file"/>
+          </label>
             
-            <label>
-              <h2 className={classes.title1}>Description</h2>
-              <textarea className={classes.textarea}></textarea>
-            </label>
-          <button> Submit </button>
-        </Grid>
+          <label>
+            <h2 className={classes.title1}>Description</h2>
+
+            <textarea className={classes.textarea}></textarea>
+          </label>
+
+          <button>Submit</button>
+      </Grid>
     </div>
     );
 };
